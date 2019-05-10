bff394f55ce21d0d093b5af02e074f6dc06626d7
jerome <jerome.laquay@hotmail.fr>
2019-05-10 17:05:15 +0200

    register request + password encoder

--- src/main/java/com/nouhoun/springboot/jwt/integration/controller/UserController.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/controller/UserController.java
@@ -2,10 +2,12 @@ package com.nouhoun.springboot.jwt.integration.controller;
 
 import com.nouhoun.springboot.jwt.integration.service.UserService;
 import com.nouhoun.springboot.jwt.integration.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,9 +22,13 @@ public class UserController {
 
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @RequestMapping(method = RequestMethod.POST, value="/register", produces={MediaType.APPLICATION_JSON_VALUE}, consumes={MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<User> create(@RequestBody User user) {
         try{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             User user2 = userService.createOrUpdate(user);
             return new ResponseEntity<>(user2, HttpStatus.OK);
         }catch(Exception e){
