e2d6ff13d768b33f6eec4bf1b449dbefdb3e4e43
jerome <jerome.laquay@hotmail.fr>
2019-05-15 20:59:46 +0200

    delete example ramdomcity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,24 +3,15 @@ INSERT INTO app_role (id, role_name, description) VALUES (2, 'ADMIN_USER', 'Admi
 
 -- USER
 -- non-encrypted password: jwtpass
-INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (1, 'John', 'Doe', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'john.doe');
-INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (2, 'Admin', 'Admin', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'admin.admin');
+INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (1, 'Jerome', 'Laquay', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'jeromelaquay');
+INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (2, 'Bernard', 'Dupondt', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'bernarddupondt');
+INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (3, 'fabien', 'Dubois', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'fabiendubois');
 
 
 INSERT INTO user_role(user_id, role_id) VALUES(1,1);
 INSERT INTO user_role(user_id, role_id) VALUES(2,1);
-INSERT INTO user_role(user_id, role_id) VALUES(2,2);
-
--- Populate random city table
-
-INSERT INTO random_city(id, name) VALUES (1, 'Bamako');
-INSERT INTO random_city(id, name) VALUES (2, 'Nonkon');
-INSERT INTO random_city(id, name) VALUES (3, 'Houston');
-INSERT INTO random_city(id, name) VALUES (4, 'Toronto');
-INSERT INTO random_city(id, name) VALUES (5, 'New York City');
-INSERT INTO random_city(id, name) VALUES (6, 'Mopti');
-INSERT INTO random_city(id, name) VALUES (7, 'Koulikoro');
-INSERT INTO random_city(id, name) VALUES (8, 'Moscow');
+INSERT INTO user_role(user_id, role_id) VALUES(3,1);
+INSERT INTO user_role(user_id, role_id) VALUES(3,2);
 
 insert into station (id,name,power,marque, organisation) values (1,'rizomm 1',22, 'G2Mobility', 'la catho');
 insert into station (id,name,power,marque, organisation) values (2,'rizomm 2',22, 'G2Mobility', 'la catho');
